7a8078d0cdf68a289685200e82a83b7ad0cec8fd
Alperen Kapusuz <63202119+alperenkapusuz@users.noreply.github.com>
2021-12-25 23:23:59 +0300

    Create q9-HappyNumber.py

---
+++ q9-HappyNumber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s=set()
+        while(1):
+            val=0
+            while(n):
+                index=n%10
+                val+=index*index
+                n//=10
+                
+            if val==1:
+                return 1
+            elif val in s:
+                return 0
+            else:
+                s.add(val)
+            n=val
+        return 0
