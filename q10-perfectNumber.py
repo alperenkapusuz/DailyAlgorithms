f03b2b556b829d920c696507209489b3f4acbfaa
Alperen Kapusuz <63202119+alperenkapusuz@users.noreply.github.com>
2021-12-26 21:41:44 +0300

    q10-accepted

---
+++ q10-perfectNumber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        ans = 1
+        if num <= 1:
+            return False
+        
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                ans += i + num//i
+        
+        return num == ans
+        
+        
